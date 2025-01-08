64f52af24023cac00902d9c2e9934225ccbe8c73
Isihaq abdullahi <isihaqabdullahi01@gmail.com>
2025-01-08 19:04:04 +0100

    feat: sign up and login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/Login/Login"; // Ensure the path matches exactly
-import SignupPage from "./pages/SingUp";
+import LoginPage from "./pages/Login"; // Ensure the path matches exactly
+import SignupPage from "./pages/SignUp";
 
 function App() {
   return (
