cf06998f6b52c850a3417ca485abfc53b8b8dd81
Isihaq abdullahi <isihaqabdullahi01@gmail.com>
2025-01-09 10:14:09 +0100

    feat: signUp

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -7,16 +7,13 @@ import { BadRequestError } from "../utils/error/custom.js";
 export const signUp = asyncWrapper(async(req, res, next) => {
     try{
         const { body } = req;
-        console.log(body);
-        const isUser = await User.findOne({ email: body.email });
-        if(isUser) {
-            console.log(isUser)
-            throw new BadRequestError("there is an existing user with this email")
+        const isUser = await User.findOne({ email: body.email});
+        if(isUser){
+            throw new BadRequestError("there an existing user with this email");
         }
         const user = await User.create(body);
         return success(res, 201, user);
     }catch(e){
         return error(res, e?.statusCode || 500, e)
-        
     }
 })
--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { signUp } from "../controllers/signUp.js";
+import { signUp } from "../controllers/auth.js";
 
 
 const router = Router();
